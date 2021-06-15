87798299c6129a2c414ab590818a7383c65d985a
Yennys <yenny1989@gmail.com>
2021-06-15 00:47:18 -0500

    PrivateRoute y login

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,20 +1,20 @@
 import { useLocation, useHistory } from "react-router-dom";
 export default function ProductsPage() {
     const location = useLocation();
-    const history = useHistory();
-        console.log(location);
+    const history = useHistory(); // Nos permite navegar entre distintas rutas
+  //console.log(location);
 
     const query = new URLSearchParams(location.search)
-    console.log(query);
-    const skip = query.get("skip") || 0
-    const limit = query.get("limit") || 15
-    console.log(skip, limit);
+    //console.log(query);
+    const skip =parseInt(query.get("skip") ) || 0 
+    const limit =parseInt(query.get("limit") ) || 15
+    //console.log(skip, limit);
 
     const handleNext = () => {
         
-        query.set("skip", 100)
-        query.set("limit",200)
-        history.push("/home")
+        query.set("skip", skip + limit) //Actualizar valor
+        //query.set("limit",200)
+        history.push({search: query.toString()}) 
     }
 
 
