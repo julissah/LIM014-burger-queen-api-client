3005a2d0c10de7677341a029ca15bbaf48ab761f
VillalobosNicole <nicolevillalobosbe@gmail.com>
2021-06-16 12:05:17 -0500

    agregando auth para el login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,21 @@
 import './Login.css';
+import { createToken } from '../services/auth';
+import React, { useState, useEffect } from 'react';
+
+
 export default function LoginPage() {
+  const [count, setCount] = useState(0);
+
+  // De forma similar a componentDidMount y componentDidUpdate
+  useEffect(() => {
+    const data = {
+      email: "email@email.com" ,
+      password: "125345"
+    }
+    createToken(data).then((res)=> {
+      console.log(res)
+    })
+  });
     return (
         <div className="login-wrapper">
         <h1>Please Log In</h1>
